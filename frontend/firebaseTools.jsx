db7035eaf14f128974bdee139a59730524839cbd
Nicolas Garnier <nivco@google.com>
2017-09-21 03:24:52 +0200

    Refactor and cleanup of Firebase Tools.

--- frontend/firebaseTools.jsx
+++ frontend/firebaseTools.jsx
@@ -13,48 +13,60 @@
  * See the License for t`he specific language governing permissions and
  * limitations under the License.
  */
-// @flow
 
-import { canUseDOM } from 'exenv';
+import * as Cookies from 'js-cookie';
 
 /**
- * Keeps the ID token in sync in the __session cookie.
+ * Keeps the ID token in sync in a cookie. While this is active, the current ID Token of the
+ * signed-in Firebase user is synced in a cookie of the given name.
+ *
+ * You can call the returned Function to stop the sync.
  *
  * @param {Object} firebaseApp - The Firebase instance that will be used.
- * @param {string} cookieName - The Firebase instance that will be used.
- * @return {Function} - The unsubscribe function.
+ * @param {string} [cookieName] - The name of the cookie that will hold the ID Token. This defaults
+ *     to '__session'.
+ * @return {Function} - The unsubscribe function. Call this function to stop the process.
  */
-export function copyIdTokenToCookie(firebaseApp, cookieName) {
-  if (canUseDOM) {
+export function keepIdTokenInCookie(firebaseApp, cookieName = '__session') {
+  if (document instanceof Object) {
     // Make sure the Firebase ID Token is always passed as a cookie.
     return firebaseApp.auth().onIdTokenChanged(user => {
       if (user) {
         user.getIdToken().then(idToken => {
-          console.log('User signed-in! ID Token:', idToken);
-          document.cookie = cookieName + '=' + idToken + ';max-age=' + (idToken ? 3600 : 0);
+          console.log('User signed-in or new ID Token for user!');
+          const existingCookieValue = Cookies.get(cookieName);
+          // If the token is not in the cookies yet we add it.
+          if (existingCookieValue !== idToken) {
+            console.log('Saving new ID Token in', cookieName, 'cookie.');
+            // Set a Cookie valid for 1h.
+            Cookies.set(cookieName, idToken, {expires: 0.04166});
+          }
         });
       } else {
         console.log('User signed-out!');
-        document.cookie = cookieName + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
+        Cookies.set(cookieName, '', {expires: 0});
       }
     });
   } else {
-    console.warn('You can only use cookies in a DOM environment.');
+    console.warn('You can only use cookies in a DOM environment. This is a NoOp');
     return () => {};
   }
 }
 
 /**
- * Returns a promise that completes when Firebase Auth is ready in the given store using react-redux-firebase.
+ * Returns a promise that completes when Firebase Auth is ready in the given store using
+ * react-redux-firebase.
  *
  * @param {Object} store - The Redux store on which we want to detect if Firebase auth is ready.
- * @param {string} [firebaseReducerAttributeName] - The attribute name of the react-redux-firebase reducer. 'firebaseState' by default.
+ * @param {string} [firebaseReducerAttributeName] - The attribute name of the react-redux-firebase
+ *     reducer. 'firebaseState' by default.
  * @return {Promise} - A promise that completes when Firebase auth is ready in the store.
  */
 export function whenAuthReady(store, firebaseReducerAttributeName = 'firebaseState') {
   const isAuthReady = store => {
     const state = store.getState();
-    const firebaseState = firebaseReducerAttributeName ? state[firebaseReducerAttributeName] : state;
+    const firebaseState = firebaseReducerAttributeName ?
+        state[firebaseReducerAttributeName] : state;
     return firebaseState && firebaseState.auth && firebaseState.auth.isLoaded;
   };
 
